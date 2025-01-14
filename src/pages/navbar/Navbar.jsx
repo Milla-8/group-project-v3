bd984d395bf33924dbb6e1efada6b43f0b4ee104
zorpid <61243974+zorpid@users.noreply.github.com>
2025-01-14 22:55:23 +0100

    Desgin changes
    Added Logo

--- src/pages/navbar/Navbar.jsx
+++ src/pages/navbar/Navbar.jsx
@@ -1,30 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.scss';
+import logo from '../../assets/BeanLogo2.png';
 
 const Navbar = () => {
-  const [isMenuOpen, setMenuOpen] = useState(false);
-
   return (
     <header className="navbar">
       <div className="navbar-logo">
-        <Link to="/">MyApp</Link>
+        <Link to="/">
+          <img src={logo} alt="MyApp Logo" className="navbar-logo-img" />
+        </Link>
       </div>
-      <nav className={`navbar-links ${isMenuOpen ? 'show' : ''}`}>
+      <nav className="navbar-links">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
       </nav>
-      <button
-        className="navbar-toggle"
-        onClick={() => setMenuOpen(!isMenuOpen)}
-        aria-label="Toggle Menu"
-      >
-        ☰
-      </button>
     </header>
   );
 };
 
 export default Navbar;
-
