50a0e39c709935a5bf0149f998a0eb0fae70b72c
CamillaEvebark <camilla.evebark@gmail.com>
2025-01-15 10:12:02 +0100

    Created Header
    Created Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.scss';
+
+export default function Header() {
+  return (
+    <div>
+        <h1>Welcome to our website</h1>
+        <p>A website about things</p>
+    </div>
+  )
+}
