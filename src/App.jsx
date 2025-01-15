f60d3473a6ceea21c6a33ac59c895d1afdd5036f
CamillaEvebark <camilla.evebark@gmail.com>
2025-01-15 10:19:03 +0100

    Updated folder for Header
    Updated folder for Header

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Routes, Route, Link } from 'react-router-dom';
 import About from './pages/about/About';
 import Navbar from './pages/navbar/Navbar';
 import Footer from './pages/footer/Footer';
+import Header from './pages/header/Header.jsx'
 
 
 function App() {
