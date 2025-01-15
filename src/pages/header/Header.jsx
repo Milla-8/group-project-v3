ae4b8cd7c524516682593669689be6f70eeb4fba
CamillaEvebark <camilla.evebark@gmail.com>
2025-01-15 12:10:35 +0100

    Updated Header
    Updated Header

--- src/pages/header/Header.jsx
+++ src/pages/header/Header.jsx
@@ -3,9 +3,9 @@ import './Header.scss';
 
 export default function Header() {
   return (
-    <div>
+    <header className="header">
         <h1>Welcome to our website</h1>
         <p>A website about things</p>
-    </div>
+    </header>
   )
 }
