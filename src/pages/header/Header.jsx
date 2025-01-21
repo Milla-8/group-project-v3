2ec4c86902e57ed8d220d517ee383ddf5f85db71
CamillaEvebark <camilla.evebark@gmail.com>
2025-01-21 19:51:54 +0100

    Updated color
    Updated color on recipes + removed background color on hover

--- src/pages/header/Header.jsx
+++ src/pages/header/Header.jsx
@@ -4,8 +4,8 @@ import './Header.scss';
 export default function Header() {
   return (
     <header className="header">
-        <h1>Welcome to our website</h1>
-        <p>A website about things</p>
+        <h1>Jelly Belly Beans</h1>
+        <p></p>
     </header>
   )
 }
