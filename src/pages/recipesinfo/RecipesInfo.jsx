d499821e8b57afd5ba186953ac062f3ad3ecb857
CamillaEvebark <camilla.evebark@gmail.com>
2025-01-21 12:43:54 +0100

    Updated recipesinfo
    Added more text to recipesinfo

--- src/pages/recipesinfo/RecipesInfo.jsx
+++ src/pages/recipesinfo/RecipesInfo.jsx
@@ -16,7 +16,7 @@ const RecipesInfo = () => {
           Whether you’re looking to bake, create fun desserts, or add a pop of sweetness to your everyday treats, we’ve got you covered.
           From cookies bursting with jelly beans to vibrant cakes and creative snacks, our recipes are designed to bring joy and a splash of color to your kitchen.
           Perfect for parties, family gatherings, or just a sweet indulgence at home, these ideas are sure to spark your creativity.
-          Browse through our collection and find your next jelly bean masterpiece today!</p>
+          Browse through our collection and find your next jelly bean masterpiece today! Click the image to find your next favorite recipe!</p>
       </div>
     </div>
   );
