48812665e6575c422e5bd7be233908aadfd3e88f
Alexander Moe Ditlevsen <amoeditlevsen@gmail.com>
2025-01-21 20:11:06 +0100

    Added reactivity to all my pages and sections

--- src/pages/factsinfo/FactsInfo.jsx
+++ src/pages/factsinfo/FactsInfo.jsx
@@ -9,7 +9,7 @@ function FactsInfo() {
             <h1>Facts</h1>
             <p>Learn interesting facts about jelly beans, some more controversial than others, and be the life of the
               party! Sharing your bean knowledge at the next family gathering will surely make 
-              you more likable and fun, or simply just read them to satisfy your own curiosity and then keep that knowledge to yourself. 
+              you more likable and fun, or simply just read them to satisfy your own curiosity and keep all that knowledge to yourself. 
               There are alot of them, a whole 99 in fact (but they're quite quick and to the point)! Click the image to the right to explore them all.      
             </p>
           </div>
