3b9a434a19f1f479fdc0bd43d3c34c322706c472
CamillaEvebark <camilla.evebark@gmail.com>
2025-01-22 11:23:53 +0100

    Updated Header
    Added picture and made header responsive

--- src/pages/header/Header.jsx
+++ src/pages/header/Header.jsx
@@ -4,8 +4,14 @@ import './Header.scss';
 export default function Header() {
   return (
     <header className="header">
+	<div className= "header-background">
+		<img src="/src/assets/bean-frame.png" alt="jelly-beans-background"/>
+	</div>
+	<div className= "header-content">
         <h1>Jelly Belly Beans</h1>
-        <p></p>
+        <p>Welcome to our sweet world!</p>
+	</div>
     </header>
+
   )
 }
