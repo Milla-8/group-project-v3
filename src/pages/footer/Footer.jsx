d67a5245e4c147e6c381d8810c58090490492bdc
Alexander Moe Ditlevsen <amoeditlevsen@gmail.com>
2025-01-23 15:02:50 +0100

    Removed gridcard borders, added new image to footer

--- src/pages/footer/Footer.jsx
+++ src/pages/footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Footer.scss';
-import logo from '../../assets/BeanLogo2.png';
+import logo from '../../assets/Jar.png';
 
 function Footer() {
     return (
