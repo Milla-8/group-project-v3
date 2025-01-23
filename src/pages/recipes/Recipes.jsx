3f949e123f4da91cc8e2abb7e31e19d32d52423f
CamillaEvebark <camilla.evebark@gmail.com>
2025-01-23 15:59:22 +0100

    Changed link from image to recipe card
    Changed link from recipe image to recipe card + removed border on hover + changed font color in recipe card

--- src/pages/recipes/Recipes.jsx
+++ src/pages/recipes/Recipes.jsx
@@ -41,25 +41,28 @@ const RecipesPage = () => {
             <h1 className="title">Jelly Beans Recipes</h1>
             <div className="recipe-grid">
                 {recipes.items.map((recipe) => (
-                    <div key={recipe.recipeId} className="recipe-card">
-                        <div className="recipe-card-image">
+
+                    <div className="recipe-card">
                         <Link
+                            key={recipe.recipeId}
                             to={`/recipes/${recipe.recipeId}`}
                             state={{ recipe }} // Send recipe data as state
                             className="recipe-link">
+                        <div className="recipe-card-image">
                             <img src={recipe.imageUrl} alt={recipe.name} />
-                        </Link>  
                         </div>
                         <div className="recipe-card-content">
                             <h3>{recipe.name}</h3>
-                            <p className="recipes-description">{recipe.description}</p><br/>
+                            <p className="recipes-description">{recipe.description}</p><br />
                             <p className="recipes-totaltime">Total time: {recipe.totalTime ? recipe.totalTime : '-'}</p>
                             <p className="recipes-amount">Amount: {recipe.makingAmount}</p>
                         </div>
+                    </Link>
                     </div>
+                   
                 ))}
-            </div>
         </div>
+        </div >
     );
 };
 
